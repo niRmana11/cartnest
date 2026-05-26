195e0f319aeb9df10741ff3ec8b9990c44078c43
niRmana11 <nirmanaherath123@gmail.com>
2026-05-26 20:41:55 +0530

    feat(frontend): initialize Vite + React + Tailwind CSS with dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1 className="text-lg text-red-500">Get started</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
